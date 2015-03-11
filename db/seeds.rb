ed59a0ca3047acf0fa08e9b0e25998798eac1b29
sagarey <saga.rey@outlook.com>
2015-03-11 16:49:35 +0800

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,9 @@ User.create!( name: "Example User",
               email: "example@railstutorial.org",
               password: "foobar",
               password_confirmation: "foobar",
-              admin: true )
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now )
 
 9.times do |n|
   name = Faker::Name.name
@@ -18,7 +20,9 @@ User.create!( name: "Example User",
   User.create!( name: name,
                 email: email,
                 password: password,
-                password_confirmation: password )
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now )
 end
 
 90.times do |n|
@@ -28,5 +32,7 @@ end
   User.create!( name: name,
                 email: email,
                 password: password,
-                password_confirmation: password )
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now )
 end
