87bbf1dc2c987fef61092d2ba6591529d5d7cb4f
sagarey <saga.rey@outlook.com>
2015-03-17 00:51:51 +0800

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'mina/git'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, '123.57.137.80'
+set :domain, '114.215.89.183'
 set :deploy_to, '/home/saga/micropost'
 set :repository, 'git@github.com:SagaRey/micropost.git'
 set :branch, 'develop'
@@ -20,7 +20,7 @@ set :branch, 'develop'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'config/environments/production.rb', 'log']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'config/environments/production.rb', 'public/uploads', 'log']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
@@ -47,6 +47,15 @@ task :setup => :environment do
 
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config/environments"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config/environments"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/public"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/public"]
+
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/public/uploads"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/public/uploads"]
 
   queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
   queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
